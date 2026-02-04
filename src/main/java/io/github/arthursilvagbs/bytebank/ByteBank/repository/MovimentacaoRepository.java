3f1a041d91490c5193aa527f7ece67f0650b3727
Arthur Silva <arthur.gbs.dev@gmail.com>
2026-02-04 15:06:20 -0300

    Criando os metodos da classe MovimentacaoService

--- src/main/java/io/github/arthursilvagbs/bytebank/ByteBank/repository/MovimentacaoRepository.java
+++ src/main/java/io/github/arthursilvagbs/bytebank/ByteBank/repository/MovimentacaoRepository.java
@@ -1,9 +1,12 @@
 package io.github.arthursilvagbs.bytebank.ByteBank.repository;
 
-import io.github.arthursilvagbs.bytebank.ByteBank.model.Cliente;
+import io.github.arthursilvagbs.bytebank.ByteBank.model.Conta;
+import io.github.arthursilvagbs.bytebank.ByteBank.model.Movimentacao;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.UUID;
 
-public interface MovimentacaoRepository extends JpaRepository<Cliente, UUID> {
+public interface MovimentacaoRepository extends JpaRepository<Movimentacao, UUID> {
+    List<Movimentacao> findAllByContaOrigem(Conta conta);
 }
--- src/main/java/io/github/arthursilvagbs/bytebank/ByteBank/service/MovimentacaoService.java
+++ src/main/java/io/github/arthursilvagbs/bytebank/ByteBank/service/MovimentacaoService.java
@@ -1,9 +1,14 @@
 package io.github.arthursilvagbs.bytebank.ByteBank.service;
 
+import io.github.arthursilvagbs.bytebank.ByteBank.model.Conta;
 import io.github.arthursilvagbs.bytebank.ByteBank.model.Movimentacao;
 import io.github.arthursilvagbs.bytebank.ByteBank.repository.MovimentacaoRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
@@ -11,6 +16,16 @@ public class MovimentacaoService {
 
     private final MovimentacaoRepository repository;
 
-    public Movimentacao
+    public Movimentacao salvar(Movimentacao movimentacao) {
+        return repository.save(movimentacao);
+    }
+
+    public Optional<Movimentacao> buscarMovimantacaoPorId(UUID id) {
+        return repository.findById(id);
+    }
+
+    public List<Movimentacao> obterMovimentacoesPorConta(Conta conta) {
+        return repository.findAllByContaOrigem(conta);
+    }
 
 }
