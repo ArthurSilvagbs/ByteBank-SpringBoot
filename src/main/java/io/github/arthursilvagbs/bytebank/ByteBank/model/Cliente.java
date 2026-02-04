4f55b4d309e933cfec7cdb06fb894bcfbf81a31a
Arthur Silva <arthur.gbs.dev@gmail.com>
2026-02-04 10:18:34 -0300

    Criando os atributos da classe Cliente, e usando anotions do Lombok pra crias getters e setters

--- src/main/java/io/github/arthursilvagbs/bytebank/ByteBank/model/Cliente.java
+++ src/main/java/io/github/arthursilvagbs/bytebank/ByteBank/model/Cliente.java
@@ -1,4 +1,34 @@
 package io.github.arthursilvagbs.bytebank.ByteBank.model;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.UUID;
+
+@Entity
+@Table(name = "cliente")
+@Getter @Setter
+@EntityListeners(AuditingEntityListener.class)
 public class Cliente {
+
+    @Id
+    @Column(name = "id", nullable = false, unique = true)
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(name = "nome", nullable = false, length = 250)
+    private String nome;
+
+    @CreatedDate
+    @Column(name = "data_cadastro")
+    private LocalDate dataCadastro;
+
+    @OneToMany(mappedBy = "cliente", fetch = FetchType.LAZY)
+    private List<Conta> contas;
+
 }
