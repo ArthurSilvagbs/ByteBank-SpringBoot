a3df1454cb2546399f01381172f674bdde0f4627
Arthur Silva <arthur.gbs.dev@gmail.com>
2026-02-05 10:59:46 -0300

    Criando os metodos GET POST na classe ClienteController. Criando as classes PessoaFisicaDTO e PessoaJuridicaDTO dentro do pacoto DTO

--- src/main/java/io/github/arthursilvagbs/bytebank/ByteBank/controller/ClienteController.java
+++ src/main/java/io/github/arthursilvagbs/bytebank/ByteBank/controller/ClienteController.java
@@ -1,4 +1,100 @@
 package io.github.arthursilvagbs.bytebank.ByteBank.controller;
 
+import io.github.arthursilvagbs.bytebank.ByteBank.controller.DTO.PessoaFisicaDTO;
+import io.github.arthursilvagbs.bytebank.ByteBank.controller.DTO.PessoaJuridicaDTO;
+import io.github.arthursilvagbs.bytebank.ByteBank.model.Cliente;
+import io.github.arthursilvagbs.bytebank.ByteBank.model.PessoaFisica;
+import io.github.arthursilvagbs.bytebank.ByteBank.model.PessoaJuridica;
+import io.github.arthursilvagbs.bytebank.ByteBank.service.ClienteService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.Optional;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/clientes")
+@RequiredArgsConstructor
 public class ClienteController {
+
+    private final ClienteService service;
+
+    @PostMapping
+    public ResponseEntity<Object> salvarPessoaFisica(@RequestBody PessoaFisicaDTO pessoaFisicaDTO) {
+
+        PessoaFisica novoCliente = pessoaFisicaDTO.mapearParaPessoaFisica();
+
+        service.salvar(novoCliente);
+
+        URI location = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(novoCliente.getId())
+                .toUri();
+
+        return ResponseEntity.created(location).build();
+    }
+
+    @PostMapping
+    public ResponseEntity<Object> salvarPessoaJuridica(@RequestBody PessoaJuridicaDTO pessoaJuridicaDTO) {
+
+        PessoaJuridica novoCliente = pessoaJuridicaDTO.mapearParaPessoaJuridica();
+
+        service.salvar(novoCliente);
+
+        URI location = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(novoCliente.getId())
+                .toUri();
+
+        return ResponseEntity.created(location).build();
+    }
+
+
+    public ResponseEntity<Object> buscarPorId(String id) {
+
+        UUID uuid = UUID.fromString(id);
+
+        Optional<Cliente> clienteEncontrado = service.obterPorId(uuid);
+
+        if (clienteEncontrado.isPresent()) {
+            Cliente clienteEntidade = clienteEncontrado.get();
+
+            if (clienteEntidade instanceof PessoaFisica pf) {
+                PessoaFisicaDTO pessoaFisicaDTO = new PessoaFisicaDTO(
+                        pf.getNome(),
+                        pf.getEmail(),
+                        pf.getTelefone(),
+                        pf.getEndereco(),
+                        pf.getTipoCliente(),
+                        pf.getCpf()
+                );
+
+                return ResponseEntity.ok(pessoaFisicaDTO);
+            }
+
+            if (clienteEntidade instanceof PessoaJuridica pj) {
+                PessoaJuridicaDTO pessoaJuridicaDTO = new PessoaJuridicaDTO(
+                        pj.getNome(),
+                        pj.getEmail(),
+                        pj.getTelefone(),
+                        pj.getEndereco(),
+                        pj.getTipoCliente(),
+                        pj.getCnpj()
+                );
+
+                return ResponseEntity.ok(pessoaJuridicaDTO);
+            }
+
+        }
+
+        return ResponseEntity.notFound().build();
+    }
 }
