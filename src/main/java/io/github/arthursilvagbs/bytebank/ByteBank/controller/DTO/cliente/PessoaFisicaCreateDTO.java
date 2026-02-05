77da6ef9cce864a09d094843ed9d0b6a1a3df999
Arthur Silva <arthur.gbs.dev@gmail.com>
2026-02-05 11:29:35 -0300

    Criando os metodos de mapeamento das classes createDTO

--- src/main/java/io/github/arthursilvagbs/bytebank/ByteBank/controller/DTO/cliente/PessoaFisicaCreateDTO.java
+++ src/main/java/io/github/arthursilvagbs/bytebank/ByteBank/controller/DTO/cliente/PessoaFisicaCreateDTO.java
@@ -1,4 +1,11 @@
 package io.github.arthursilvagbs.bytebank.ByteBank.controller.DTO.cliente;
 
-public record PessoaFisicaCreateDTO() {
+import io.github.arthursilvagbs.bytebank.ByteBank.model.PessoaFisica;
+import io.github.arthursilvagbs.bytebank.ByteBank.model.TipoCliente;
+
+public record PessoaFisicaCreateDTO(String nome, String email, String telefone, String endereco, TipoCliente tipoCliente, String cpf) {
+
+    public PessoaFisica mapearParaPessoaFisica() {
+        return new PessoaFisica(this.nome, this.email, this.telefone, this.endereco, this.tipoCliente, this.cpf);
+    }
 }
--- src/main/java/io/github/arthursilvagbs/bytebank/ByteBank/controller/DTO/cliente/PessoaJuridicaCreateDTO.java
+++ src/main/java/io/github/arthursilvagbs/bytebank/ByteBank/controller/DTO/cliente/PessoaJuridicaCreateDTO.java
@@ -1,4 +1,11 @@
 package io.github.arthursilvagbs.bytebank.ByteBank.controller.DTO.cliente;
 
-public record PessoaJuridicaCreateDTO() {
+import io.github.arthursilvagbs.bytebank.ByteBank.model.PessoaFisica;
+import io.github.arthursilvagbs.bytebank.ByteBank.model.TipoCliente;
+
+public record PessoaJuridicaCreateDTO(String nome, String email, String telefone, String endereco, TipoCliente tipoCliente, String cpf) {
+
+    public PessoaFisica mapearParaPessoaFisica() {
+        return new PessoaFisica(this.nome, this.email, this.telefone, this.endereco, this.tipoCliente, this.cpf);
+    }
 }
