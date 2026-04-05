b857f3ada181b6d8857f1241012f8bd05e4525ba
Arthur Silva <arthur.gbs.dev@gmail.com>
2026-04-05 11:42:02 -0300

    Criando a documentação do controller de Cliente

--- src/main/java/io/github/arthursilvagbs/bytebank/ByteBank/controller/ClienteController.java
+++ src/main/java/io/github/arthursilvagbs/bytebank/ByteBank/controller/ClienteController.java
@@ -5,6 +5,10 @@ import io.github.arthursilvagbs.bytebank.ByteBank.model.Cliente;
 import io.github.arthursilvagbs.bytebank.ByteBank.model.PessoaFisica;
 import io.github.arthursilvagbs.bytebank.ByteBank.model.PessoaJuridica;
 import io.github.arthursilvagbs.bytebank.ByteBank.service.ClienteService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -17,10 +21,18 @@ import java.util.UUID;
 @RestController
 @RequestMapping("/clientes")
 @RequiredArgsConstructor
+@Tag(name = "Clientes", description = "Operações relacionadas à clientes")
 public class ClienteController {
 
     private final ClienteService service;
 
+    @Operation(description = "Criar um cliente Pessoa Física")
+    @ApiResponses(
+            value = {
+                    @ApiResponse(responseCode = "201", description = "Cria o cliente Pessoa Física e devolve a url cliente criado no response"),
+                    @ApiResponse(responseCode = "400", description = "Dados inválidos na request")
+            }
+    )
     @PostMapping("/pf")
     public ResponseEntity<Object> salvarPessoaFisica(@RequestBody PessoaFisicaCreateDTO dto) {
 
@@ -37,6 +49,13 @@ public class ClienteController {
         return ResponseEntity.created(location).build();
     }
 
+    @Operation(description = "Criar um cliente Pessoa Juridica")
+    @ApiResponses(
+            value = {
+                    @ApiResponse(responseCode = "201", description = "Cria o cliente Pessoa Juridica e devolve a url cliente criado no response"),
+                    @ApiResponse(responseCode = "400", description = "Dados inválidos na request")
+            }
+    )
     @PostMapping("/pj")
     public ResponseEntity<Object> salvarPessoaJuridica(@RequestBody PessoaJuridicaCreateDTO dto) {
 
@@ -53,7 +72,14 @@ public class ClienteController {
         return ResponseEntity.created(location).build();
     }
 
-
+    @Operation(description = "Buscar um cliente no banco via ID")
+    @ApiResponses(
+            value = {
+                    @ApiResponse(responseCode = "200", description = "Retorna o cliente encontrado no body da response"),
+                    @ApiResponse(responseCode = "400", description = "Dados inválidos na request"),
+                    @ApiResponse(responseCode = "404", description = "ID informado não foi encontrado no banco de dados")
+            }
+    )
     @GetMapping("{id}")
     public ResponseEntity<Object> buscarPorId(@PathVariable("id") String id) {
 
@@ -97,6 +123,14 @@ public class ClienteController {
         return ResponseEntity.notFound().build();
     }
 
+    @Operation(description = "Atualizar um cliente existente no banco de dados, buscando-o via ID")
+    @ApiResponses(
+            value = {
+                    @ApiResponse(responseCode = "200", description = "Encontra o cliente via ID, e atualiza os dados dele, com os novos dados informados dentro do body da request"),
+                    @ApiResponse(responseCode = "400", description = "Dados inválidos na request"),
+                    @ApiResponse(responseCode = "404", description = "ID do cliente não encontrado no banco de dados, cliente não encontrado")
+            }
+    )
     @PutMapping("{id}")
     public ResponseEntity<Object> atualizar(@PathVariable("id") String id, @RequestBody ClienteUpdateDTO dto) {
         UUID idCliente = UUID.fromString(id);
@@ -117,6 +151,14 @@ public class ClienteController {
         return ResponseEntity.ok().build();
     }
 
+    @Operation(description = "Atualizar um cliente existente no banco de dados, buscando-o via ID")
+    @ApiResponses(
+            value = {
+                    @ApiResponse(responseCode = "204", description = "Encontra o cliente via ID, e deleta ele, retornando que ele não existe mais"),
+                    @ApiResponse(responseCode = "400", description = "Dados inválidos na request"),
+                    @ApiResponse(responseCode = "404", description = "ID do cliente não encontrado no banco de dados, cliente não encontrado")
+            }
+    )
     @DeleteMapping("{id}")
     public ResponseEntity<Object> deletar(@PathVariable("id") String id){
         UUID idCliente = UUID.fromString(id);
