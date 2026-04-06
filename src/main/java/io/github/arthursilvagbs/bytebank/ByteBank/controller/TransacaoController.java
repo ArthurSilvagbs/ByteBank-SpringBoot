9c1ae52b8ee364a7ee36a946abf832ea607f2fdb
Arthur Silva <arthur.silva@eventos.local>
2026-04-06 15:02:40 -0300

    Implentando o Swagger na api de transacao

--- src/main/java/io/github/arthursilvagbs/bytebank/ByteBank/controller/TransacaoController.java
+++ src/main/java/io/github/arthursilvagbs/bytebank/ByteBank/controller/TransacaoController.java
@@ -6,9 +6,14 @@ import io.github.arthursilvagbs.bytebank.ByteBank.mappers.TransacaoMapper;
 import io.github.arthursilvagbs.bytebank.ByteBank.model.Transacao;
 import io.github.arthursilvagbs.bytebank.ByteBank.service.ContaService;
 import io.github.arthursilvagbs.bytebank.ByteBank.service.TransacaoService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -27,6 +32,21 @@ public class TransacaoController {
 
    private final TransacaoMapper mapper;
 
+   @Operation(description = "Efetuar um saque")
+   @ApiResponses(
+      value = {
+         @ApiResponse(
+            responseCode = "201",
+            description = "Saque efetuado com sucesso",
+            content = @Content
+         ),
+         @ApiResponse(
+            responseCode = "400",
+            description = "Dados inválidos na request"
+         )
+      }
+   )
+   @PostMapping("/saque")
    public ResponseEntity<?> saque(@RequestBody TransacaoCreateDTO dto) {
 
       Transacao transacao = transacaoService.saque(dto);
@@ -40,6 +60,21 @@ public class TransacaoController {
       return ResponseEntity.created(location).build();
    }
 
+   @Operation(description = "Efetuar um deposito")
+   @ApiResponses(
+      value = {
+         @ApiResponse(
+            responseCode = "201",
+            description = "Deposito efetuado com sucesso",
+            content = @Content
+         ),
+         @ApiResponse(
+            responseCode = "400",
+            description = "Dados inválidos na request"
+         )
+      }
+   )
+   @PostMapping("/deposito")
    public ResponseEntity<?> deposito(@RequestBody TransacaoCreateDTO dto) {
 
       Transacao transacao = transacaoService.deposito(dto);
@@ -53,6 +88,21 @@ public class TransacaoController {
       return ResponseEntity.created(location).build();
    }
 
+   @Operation(description = "Efetuar uma transferência")
+   @ApiResponses(
+      value = {
+         @ApiResponse(
+            responseCode = "201",
+            description = "Transferência efetuada com sucesso",
+            content = @Content
+         ),
+         @ApiResponse(
+            responseCode = "400",
+            description = "Dados inválidos na request"
+         )
+      }
+   )
+   @PostMapping("/transferencia")
    public ResponseEntity<?> transferencia(@RequestBody TransferenciaCreateDTO dto) {
 
       Transacao transacao = transacaoService.transferencia(dto);
@@ -65,7 +115,5 @@ public class TransacaoController {
 
       return ResponseEntity.created(location).build();
    }
-
-
 
 }
