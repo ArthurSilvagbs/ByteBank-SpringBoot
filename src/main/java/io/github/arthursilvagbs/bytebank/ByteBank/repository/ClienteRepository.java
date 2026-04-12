6b5747c7da95c3c9e817671a07ff05e591db71d9
Arthur Silva <arthur.gbs.dev@gmail.com>
2026-04-12 15:35:09 -0300

    Colocando validação de cadastro duplicado

--- src/main/java/io/github/arthursilvagbs/bytebank/ByteBank/repository/ClienteRepository.java
+++ src/main/java/io/github/arthursilvagbs/bytebank/ByteBank/repository/ClienteRepository.java
@@ -2,6 +2,8 @@ package io.github.arthursilvagbs.bytebank.ByteBank.repository;
 
 import io.github.arthursilvagbs.bytebank.ByteBank.model.Cliente;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 import java.util.UUID;
@@ -9,4 +11,13 @@ import java.util.UUID;
 public interface ClienteRepository extends JpaRepository<Cliente, UUID> {
 
     Optional<Cliente> findByNome(String nome);
+
+    Optional<Cliente> findByEmail(String email);
+
+    @Query("SELECT p FROM PessoaFisica p WHERE p.cpf = :cpf")
+    Optional<Cliente> findByCpf(@Param("cpf") String cpf);
+
+    @Query("SELECT p FROM PessoaJuridica p WHERE p.cnpj = :cnpj")
+    Optional<Cliente> findByCnpj(@Param("cnpj") String cnpj);
+
 }
--- src/main/java/io/github/arthursilvagbs/bytebank/ByteBank/service/ClienteService.java
+++ src/main/java/io/github/arthursilvagbs/bytebank/ByteBank/service/ClienteService.java
@@ -3,9 +3,9 @@ package io.github.arthursilvagbs.bytebank.ByteBank.service;
 import io.github.arthursilvagbs.bytebank.ByteBank.DTO.cliente.PessoaFisicaCreateDTO;
 import io.github.arthursilvagbs.bytebank.ByteBank.DTO.cliente.PessoaJuridicaCreateDTO;
 import io.github.arthursilvagbs.bytebank.ByteBank.exceptions.OperacaoNaoPermitidaException;
+import io.github.arthursilvagbs.bytebank.ByteBank.exceptions.RegistroDuplicadoException;
 import io.github.arthursilvagbs.bytebank.ByteBank.mappers.ClienteMapper;
 import io.github.arthursilvagbs.bytebank.ByteBank.model.Cliente;
-import io.github.arthursilvagbs.bytebank.ByteBank.model.TipoCliente;
 import io.github.arthursilvagbs.bytebank.ByteBank.repository.ClienteRepository;
 import io.github.arthursilvagbs.bytebank.ByteBank.repository.ContaRepository;
 import lombok.RequiredArgsConstructor;
@@ -19,48 +19,56 @@ import java.util.UUID;
 @RequiredArgsConstructor
 public class ClienteService {
 
-   private final ClienteRepository respository;
+   private final ClienteRepository repository;
    private final ContaRepository contaRepository;
    private final ClienteMapper mapper;
 
    public Cliente salvarPessoaFisica(PessoaFisicaCreateDTO dto) {
+      if (repository.findByCpf(dto.cpf()).isPresent() || repository.findByEmail(dto.email()).isPresent()) {
+         throw new RegistroDuplicadoException("Cliente já cadastrado");
+      }
+
       Cliente cliente = mapper.mapearParaPessoaFisica(dto);
-      return respository.save(cliente);
+      return repository.save(cliente);
    }
 
    public Cliente salvarPessoaJuridica(PessoaJuridicaCreateDTO dto) {
+      if (repository.findByCpf(dto.cnpj()).isPresent() || repository.findByEmail(dto.email()).isPresent()) {
+         throw new RegistroDuplicadoException("Cliente já cadastrado");
+      }
+
       Cliente cliente = mapper.mapearParaPessoaJuridica(dto);
-      return respository.save(cliente);
+      return repository.save(cliente);
    }
 
    public void atualizar(Cliente cliente) {
       if (cliente.getId() == null) {
          throw new IllegalArgumentException("Para atualizar um cliente, é ncessário que ele esteja cadastrado.");
       }
-      respository.save(cliente);
+      repository.save(cliente);
    }
 
    public Optional<Cliente> obterPorId(UUID id) {
-      return respository.findById(id);
+      return repository.findById(id);
    }
 
    public void deletar(Cliente cliente) {
       if (possuiConta(cliente)) {
          throw new OperacaoNaoPermitidaException("O cliente não pode ser deletado, pois ainda existem contas vinculadas a ele.");
       }
-      respository.delete(cliente);
+      repository.delete(cliente);
    }
 
    public Optional<Cliente> pesquisaPorNome(String nome) {
-      return respository.findByNome(nome);
+      return repository.findByNome(nome);
    }
 
    public Optional<Cliente> pesquisaPorId(UUID id) {
-      return respository.findById(id);
+      return repository.findById(id);
    }
 
    public List<Cliente> obterTodosOsClientes() {
-      return respository.findAll();
+      return repository.findAll();
    }
 
    public boolean possuiConta(Cliente cliente) {
